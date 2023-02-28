24a9fe7925dbc86d3baa885e4ac480a21be6de46
JOAO PEDROZO <jp.pedrozo@hotmail.com>
2023-02-28 09:37:40 -0300

    first: change schema

--- src/server/api/routers/reviews.ts
+++ src/server/api/routers/reviews.ts
@@ -6,7 +6,8 @@ import {
   protectedProcedure,
 } from "~/server/api/trpc";
 import { prisma } from "~/server/db";
-import { appRouter } from "../root";
+import { Configuration, OpenAIApi } from "openai";
+import { env } from "~/env.mjs";
 
 export const reviewsRouter = createTRPCRouter({
   hello: publicProcedure
@@ -46,8 +47,38 @@ export const reviewsRouter = createTRPCRouter({
 
   processReviews: publicProcedure
     .input(z.object({ id: z.string() }))
-    .mutation(async () => {
-      console.log(123);
+    .mutation(async ({ input }) => {
+      const product = await prisma.product.findUnique({
+        where: {
+          id: input.id,
+        },
+      });
+
+      const configuration = new Configuration({
+        apiKey: env.OPEN_AI_API_KEY,
+      });
+      const openai = new OpenAIApi(configuration);
+      const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `Atue como um gerador de avaliações de produto.
+         Eu irei lhe fornecer as descrições de um produto e você irá me fornecer 10 
+         avaliações para esse produto, bem como uma nota para o produto de 1.0 a 5.0, aonde 
+          85% das avaliações devem ser acima de 4. Diferencie radicalmente o campo ${"`review`"} de cada avaliação. Me envie o resultado em
+            formato JSON, seguindo essa estrutura: { "reviews": [ "review": <text>, "rating": <number> ] }. 
+            A descrição do produto é: "${product?.description}”`,
+        max_tokens: 2000,
+        temperature: 0.4,
+      });
+
+      console.log(response.data);
+
+      let text = response.data.choices[0]?.text!;
+
+      if (text.startsWith(".")) {
+        text = text.substring(1);
+      }
+
+      const reviews = JSON.parse(text);
     }),
 
   getProductInProductPage: publicProcedure
